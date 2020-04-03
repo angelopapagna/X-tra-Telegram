089f6a5b8126c8f3ededb5f3a4054299954f9c23
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-04-03 22:37:52 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -24,6 +24,8 @@ class Var(object):
     G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
+    # Send .get_id in any channel to fill this value.
+    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", None))
     MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 3))
     if AUTH_TOKEN_DATA != None:
         if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
